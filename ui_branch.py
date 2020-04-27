ec9be7634c9c4bebf76d94fa42a639153fdfbf39
Tirtha Kharel <tkharel21@gmail.com>
2020-04-26 22:47:08 -0500

    buttons ready and text box

--- ui_branch.py
+++ ui_branch.py
@@ -1,5 +1,6 @@
 #uses the graphics library pygame
 import pygame
+vec = pygame.math.Vector2
 
 
 #this contains code to modify specific letters stored in individual cells
@@ -36,6 +37,81 @@ class Grid():
     def select(self, x, y, screen):
         self.cells[x][y].select(screen)
 
+#class for buttons
+class Button():
+  def __init__(self, x, y, width, height, bg_color, text, font_size=25, text_color=(0,0,0)):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.pos = vec(x, y)
+    self.size = vec(width, height)
+    self.surface = pygame.Surface((width, height))
+    self.color = bg_color
+    self.text = text
+    self.font_size = font_size
+    self.font = pygame.font.SysFont('sfprodisplayregularotf', self.font_size)
+    self.text_color = text_color
+
+  def draw(self, window):
+      self.surface.fill(self.color)
+      text = self.font.render(self.text, True, self.text_color)
+      text_height = text.get_height()
+      text_width = text.get_width()
+      self.surface.blit(text, (((self.width - text_width)//2), (self.height - text_height)//2))
+      window.blit(self.surface, self.pos)
+
+  def check_click(self, pos):
+      return pos[0] > self.x and pos[0] < self.x + self.width and pos[1] > self.y and pos[1] < self.y + self.height
+
+#class for text input box
+class TextBox():
+  def __init__(self, x, y, width, height, bg_color=(124, 124, 124), active_color=(255,255,255), font_size=25):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.bg_color = bg_color
+    self.pos = vec(x, y)
+    self.size = vec(width, height)
+    self.surface = pygame.Surface((width, height))
+    self.active_color = active_color
+    self.active = False
+    self.text = ""
+    self.font_size = font_size
+    self.font = pygame.font.SysFont('sfprodisplayregularotf', self.font_size)
+
+  def draw(self, window):
+    if not self.active:
+      self.surface.fill(self.bg_color)
+      text = self.font.render(self.text, False, (255, 255, 255))
+      text_height = text.get_height()
+      text_width = text.get_width()
+      self.surface.blit(text, (0, (self.height - text_height)//2))
+    else:
+      self.surface.fill(self.active_color)
+      text = self.font.render(self.text, False, (0, 0, 0))
+      text_height = text.get_height()
+      text_width = text.get_width()
+      self.surface.blit(text, (0, (self.height - text_height)//2))
+
+    window.blit(self.surface, self.pos)
+
+  def check_click(self, pos):
+    if pos[0] > self.x and pos[0] < self.x + self.width:
+      if pos[1] > self.y and pos[1] < self.y + self.height:
+        self.active = True
+      else:
+        self.active = False
+    else:
+      self.active = False
+    return self.active
+  
+  def add_text(self, key):
+    text = list(self.text)
+    text.append(chr(key))
+    self.text = ''.join(text)
+
 # the main game engine is here. Will have to restructure a bit
 
 #initialize the engine
@@ -69,26 +145,24 @@ done = False
 
 clock = pygame.time.Clock()
 
+
+
+
+#array for textbox click check
+textboxes = []
+textboxes.append(TextBox(0, 350, 800, 50))
+
+#array for buttons (also level selector)
+buttons = []
+buttons.append(Button(240, 220, 80, 40, RED, "LEVEL 1", 20))
+buttons.append(Button(360, 220, 80, 40, RED, "LEVEL 2", 20))
+buttons.append(Button(480, 220, 80, 40, RED, "LEVEL 3", 20))
+
 #checks if the mouse button is clicked, initializes as true
 #todo: modify code to not require this
 clicked = False
 intro = True
-
-title = titleFont.render("Vim Game", True, (0, 200, 0))
-subtitle = subtitleFont.render("Click anywhere to begin", True, (26,134,230))
-titlePos = title.get_rect()
-sTitlePos = subtitle.get_rect()
-
-intro_background = pygame.Surface(screen.get_size())
-intro_background = intro_background.convert()
-intro_background.fill((250, 250, 250))
-titlePos.centerx = intro_background.get_rect().centerx
-titlePos.centery = intro_background.get_rect().centery - 20
-sTitlePos.centerx = intro_background.get_rect().centerx
-sTitlePos.centery = titlePos.centery + 30
-
-intro_background.blit(title, titlePos)
-intro_background.blit(subtitle, sTitlePos)
+intro_clicked = False
 
 #selected letter
 
@@ -98,20 +172,56 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN and intro:
+        if event.type == pygame.MOUSEBUTTONDOWN and intro:
             intro = False
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            clicked = True
-
-        # more events can be added for functions
-        #todo:
-        # add vim command functionality
+            intro_clicked = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            clicked = not textboxes[0].check_click(pygame.mouse.get_pos())
+        if event.type == pygame.KEYDOWN:
+            if textboxes[0].active:
+                textboxes[0].add_text(event.key)
 
     if intro:
+        title = titleFont.render("Vim Game", True, (0, 200, 0))
+        subtitle = subtitleFont.render("Click anywhere to begin", True, (26,134,230))
+        subtitle2 = subtitleFont.render("Or, click on one of the level buttons", True, (26,134,230))
+        titlePos = title.get_rect()
+        sTitlePos = subtitle.get_rect()
+        s2TitlePos = subtitle2.get_rect()
+
+        intro_background = pygame.Surface(screen.get_size())
+        intro_background = intro_background.convert()
+        intro_background.fill((250, 250, 250))
+        titlePos.centerx = intro_background.get_rect().centerx
+        titlePos.centery = intro_background.get_rect().centery - 60
+        sTitlePos.centerx = intro_background.get_rect().centerx
+        sTitlePos.centery = titlePos.centery + 30
+        s2TitlePos.centerx = titlePos.centerx
+        s2TitlePos.centery = sTitlePos.centery + 15
+
+        intro_background.blit(title, titlePos)
+        intro_background.blit(subtitle, sTitlePos)
+        intro_background.blit(subtitle2, s2TitlePos)
+
+        for button in buttons:
+          button.draw(intro_background)
+
         screen.blit(intro_background, (0,0))
 
+
+    # more events can be added for functions
+    #todo:
+    # add vim command functionality
+    if intro_clicked:
+        pos = pygame.mouse.get_pos()
+        x = pos[0]
+        y = pos[1]
+        intro_clicked = False
+        # need to have level loader here
+
     #checks to see if we clicked on something
-    if clicked:
+    
+    if not intro:
         screen.fill(WHITE)
         pos = pygame.mouse.get_pos()
         x = pos[0]
@@ -121,15 +231,18 @@ while not done:
         g.show(screen)
         g.select(2,3, screen)
 
-        #helper to see where I click, remove later
-        text = font.render("click!", True, GREEN)
-        screen.blit(text, [x, y])
-
-        #rather than detect a keypress up, we just allow a single action per event
-        clicked = False
+        if clicked:
+            #helper to see where I click, remove later
+            text = font.render("click!", True, GREEN)
+            screen.blit(text, [x, y])
+            #rather than detect a keypress up, we just allow a single action per event
+            clicked = False
+        
+        for textbox in textboxes:
+            textbox.draw(screen)
 
     # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
+    pygame.display.update()
     # Limit frames per second
     clock.tick(60)
 
